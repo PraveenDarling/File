855e90f8541c4f1930d5ecec1fc368c4756be94b
PraveenDarling <86154871+PraveenDarling@users.noreply.github.com>
2021-06-19 20:20:40 +0530

    created ByteShortIntLong

---
+++ ByteShortIntLong.java
@@ -0,0 +1,13 @@
+public class ByteShortIntLong
+{
+public static void main(String[] arg)
+{
+	System.out.println("!!----INTEGER SIZE OF PRIMITIVE TYPES-----!!");
+	 //int myValue = 10000;
+	int myMinIntValue = Integer.MIN_VALUE;
+	int myMaxIntValue = Integer.MAX_VALUE;
+	System.out.println("INTEGER MINIMUM VALUE = " +  myMinIntValue);
+	System.out.println("INTEGER MAXIMUM VALUE = " +  myMaxIntValue);
+	System.out.println("Busted MAXIMUM VALUE = " +  ( myMaxIntValue + 1));
+	System.out.println("Busted MAXIMUM VALUE = " +  (myMaxIntValue - 1));
+	
